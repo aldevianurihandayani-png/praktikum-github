9560b5fcb679e343f385c28145a692b080cb9897
Noorma <noorma@mhs.politala.ac.id>
2025-09-02 10:21:17 +0800

    menambahkan kode baru

---
+++ kodingan.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <title>Tentang kelompok 1</title>
+</head>
+<body>
+  <h1>Tentang kami</h1>
+  <p>Halo, kami dari kelompok 1 pbl, mahasiswa dikampus politala kelas 3E.</p>
+  <p>Saya sedang belajar Git dan GitHub untuk praktikum.</p>
+  <a href="index.html">Kembali ke Halaman Utama</a>
+</body>
+</html>
